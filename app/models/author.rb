afac848692707afb99ad9fd2ee815f2579a4d67d
Bernadette Davis <bsd231@nyu.edu>
2019-11-30 18:24:30 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
-  
+
   validates :phone_number, format: { with: /[0-9]/, message: "Only allows numbers" }
   validates :phone_number, length: { is: 6 }
 end
